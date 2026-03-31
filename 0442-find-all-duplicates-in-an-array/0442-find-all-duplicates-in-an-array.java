35c5982311f2c4d58e03c4633a08fa2bd332f172
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-01 06:14:06 +0700

    Time: 6 ms (91.15%), Space: 57.7 MB (78.27%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             int val = Math.abs(nums[i]);
             int index = val - 1;
-            if(nums[index] < 0){
-                    res.add(Math.abs(nums[index]));
+            if(nums[index] > 0){
+                nums[index] = - nums[index];
             }
-            else nums[index] = -nums[index];
+            else res.add(Math.abs(val));
         }
         return res;
     }
