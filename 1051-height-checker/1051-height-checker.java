55ca627b086a5d22d7bd67e01ad7c41f2bd4fa0f
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-02 22:39:08 +0700

    Time: 0 ms (100%), Space: 43.4 MB (66.24%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,11 +1,14 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int copy[] = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(copy);
-        int cnt = 0;
-        for(int i = 0; i < heights.length; i++){
-            if(heights[i] != copy[i]) ++cnt;
+        int cnt[] = new int[101];
+        for(int num : heights) cnt[num]++;
+        int curHeight = 0;
+        int countMatch = 0;
+        for(int num : heights){
+            while(cnt[curHeight] == 0) ++curHeight;
+            if(curHeight != num) countMatch++;
+            cnt[curHeight] --;
         }
-        return cnt;
+        return countMatch;
     }
 }
