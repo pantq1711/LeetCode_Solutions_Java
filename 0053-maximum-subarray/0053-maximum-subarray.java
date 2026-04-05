193584fb61fe1cebac05e023ea9b16b8b50096aa
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-05 21:55:41 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum1 = 0, sum2 = Integer.MIN_VALUE, tmp = Integer.MIN_VALUE;
-        boolean check = false;
-        for(int num : nums){
-            if(num >= 0) check = true;
-            tmp = Math.max(tmp, num);
-            sum1 += num;
-            if(sum1 < 0) sum1 = 0;
-            sum2 = Math.max(sum1, sum2);
+        int curSum = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            curSum = Math.max(nums[i], curSum + nums[i]);
+            maxSum = Math.max(maxSum, curSum);
         }
-        return check ? sum2 : tmp;
+        return maxSum;
     }
 }
