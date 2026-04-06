8933565970928f44ae50b82e4c17dc6f20f319b2
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-06 21:47:56 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0, j = 0, k = 0;
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] == nums2[j]){
-                nums1[k++] = nums2[j++];
-                i++;
-            }
-            else if(nums1[i] > nums2[j]){
-                j++;
-            }
-            else i++;
+     public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums1){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        int left = 0;
+        for(int num : nums2){
+            if(map.containsKey(num) && map.get(num) > 0){
+                nums1[left++] = num;
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, left);
     }
 }
