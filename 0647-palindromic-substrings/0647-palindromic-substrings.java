cc2a34bb18cc469618aab595665d0665f4070b5e
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-08 15:52:23 +0700

    Time: 5 ms (97.63%), Space: 42.9 MB (74.46%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++){
+            cnt += expand(s, i, i);
+            cnt += expand(s, i, i + 1);
+        }
+        return cnt;
+    }
+
+    public int expand(String s,  int l, int r){
+        int cnt = 0;
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            cnt++;
+            l--;
+            r++;
+        }
+        return cnt;
+    }
+}
