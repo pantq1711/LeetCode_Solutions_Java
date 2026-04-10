36609367d182662d4e67d33b30c31b22ef7a2afc
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-10 14:22:11 +0700

    Time: 5 ms (97.63%), Space: 42.7 MB (92.6%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -2,13 +2,13 @@ class Solution {
     public int countSubstrings(String s) {
         int cnt = 0;
         for(int i = 0; i < s.length(); i++){
-            cnt += expand(s, i, i);
-            cnt += expand(s, i, i + 1);
+            cnt += expandAroundCenter(s, i, i);
+            cnt += expandAroundCenter(s, i, i + 1);
         }
         return cnt;
     }
 
-    public int expand(String s,  int l, int r){
+    public int expandAroundCenter(String s, int l, int r){
         int cnt = 0;
         while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
             cnt++;
@@ -17,4 +17,5 @@ class Solution {
         }
         return cnt;
     }
+
 }
