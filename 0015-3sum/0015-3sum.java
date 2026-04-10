4b92d22affa0eaef131a32fa87576f34e65dfd7a
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-10 07:05:17 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,6 +3,8 @@ class Solution {
         Set<List<Integer>> res = new HashSet<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++){
+                        if(i > 0 && nums[i] == nums[i - 1]) continue;
+
             int left = i + 1, right = nums.length - 1;
             while(left < right){
                 if(nums[i] + nums[left] + nums[right] == 0){
