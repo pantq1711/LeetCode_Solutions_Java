a691972601bd780cfeccc1185faf5d626a0b93e5
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-14 10:47:49 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,13 +1,16 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1, right = nums.length - 1;
             while(left < right){
                 if(nums[left] + nums[right] + nums[i] == 0){
-                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    map.computeIfAbsent(i, k -> new ArrayList<>())
+                            .add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < nums.length - 1 && nums[left] == nums[left + 1]) left++;
+                    while(right > 0 && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 }
@@ -17,6 +20,10 @@ class Solution {
                 else left++;
             }
         }
-        return set.stream().toList();
+        List<List<Integer>> result = new ArrayList<>();
+        for (List<List<Integer>> triplets : map.values()) {
+            result.addAll(triplets);
+        }
+        return result;
     }
 }
