23384b2c643461dd17bf5eed020f10d83df8a90e
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-16 22:06:22 +0700

    Time: 3 ms (23.51%), Space: 48.1 MB (11.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int stepCount = nums[0];
-        if(nums.length <= 1) return true;
-        if(stepCount == 0) return false;
+        int stepsCount = nums[0];
+        if(nums.length == 1) return true;
+        if(nums[0] == 0) return false;
         for(int i = 1; i < nums.length - 1; i++){
-            stepCount--;
-            stepCount = Math.max(stepCount, nums[i]);
-            if(stepCount <= 0) return false;
+            stepsCount--;
+            stepsCount = Math.max(stepsCount, nums[i]);
+            if(stepsCount <= 0) return false;
+
         }
         return true;
     }
