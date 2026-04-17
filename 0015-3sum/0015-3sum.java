d22cddbe35848ae7ccccb0869007f57ac2315621
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-18 06:18:12 +0700

    Time: 34 ms (60.38%), Space: 58.9 MB (86.7%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer, List<List<Integer>>> map = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1, right = nums.length - 1;
             while(left < right){
-                if(nums[left] + nums[right] + nums[i] == 0){
-                    map.computeIfAbsent(i, k -> new ArrayList<>())
-                            .add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while(left < nums.length - 1 && nums[left] == nums[left + 1]) left++;
-                    while(right > 0 && nums[right] == nums[right - 1]) right--;
+                if(nums[i] + nums[left] + nums[right] == 0)
+                {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 }
-                else if(nums[left] + nums[right] + nums[i] > 0){
+                else if(nums[i] + nums[left] + nums[right] > 0){
                     right--;
                 }
                 else left++;
             }
         }
-        List<List<Integer>> result = new ArrayList<>();
-        for (List<List<Integer>> triplets : map.values()) {
-            result.addAll(triplets);
-        }
-        return result;
+        return res;
     }
+
 }
