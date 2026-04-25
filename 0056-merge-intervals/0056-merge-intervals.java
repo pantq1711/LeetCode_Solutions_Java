3bd4552ccb7f0b840f4e6ee8ed2e39ace6dbe0b9
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-25 21:08:00 +0700

    Time: 12 ms (10.08%), Space: 49.4 MB (14.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+                Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         int left = 0;
         for(int right = 1; right < intervals.length; right++){
-            if(intervals[right][0] > intervals[left][1]){
+            int start_next = intervals[right][0];
+            if(intervals[left][1] < start_next){
                 left++;
                 intervals[left] = intervals[right];
             }
-            else {
-                intervals[left][1] = Math.max(intervals[left][1], intervals[right][1]);
-            }
+            else intervals[left][1] = Math.max(intervals[right][1], intervals[left][1]);
         }
         return Arrays.copyOfRange(intervals, 0, left + 1);
     }
