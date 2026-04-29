c692aeea62bb4cb74689b29944b9b3af38999c1d
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-29 09:49:10 +0700

    Time: 2 ms (99.95%), Space: 45.6 MB (79.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length() > s.length()) return "";
         int[] cnt = new int[128];
-        String res = "";
-        int count = 0;
         for(char c : t.toCharArray()){
             cnt[c]++;
         }
-        int left = 0, right = 0;
-        int minLen = Integer.MAX_VALUE, minStart = 0;
+        int count = 0;
+        int left = 0;
+        int right = 0;
+        int startIndex = 0, minLen = Integer.MAX_VALUE;
         while(right < s.length()){
             char c = s.charAt(right);
             cnt[c]--;
-            if(cnt[c] >= 0){
-                count++;
-            }
+            if(cnt[c] >= 0) count++;
             while(count == t.length()){
-if (right - left + 1 < minLen) {
-                minLen = right - left + 1;
-                minStart = left; 
-            }
-                char left_window = s.charAt(left);
-                cnt[left_window]++;
-                if(cnt[left_window] == 1) count--;
+                if(right - left + 1 < minLen){
+                    minLen = right - left + 1;
+                    startIndex = left;
+                }
+                cnt[s.charAt(left)]++;
+                if(cnt[s.charAt(left)] == 1) count--;
                 left++;
             }
             right++;
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        return (minLen == Integer.MAX_VALUE)  ? "" : s.substring(startIndex,startIndex + minLen);
+
     }
 }
