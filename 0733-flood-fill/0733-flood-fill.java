6eb7843f2ec1266c0e94f39474d089832b1374d6
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-30 09:43:15 +0700

    Time: 0 ms (100%), Space: 46.7 MB (78.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,17 @@
 class Solution {
-    static int[] dx = {-1, 1, 0, 0};
+        static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    public void dfs(int [][]grid, int i, int j, int oldColor, int newColor){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != oldColor) return ;
+    public void dfs(int[][] grid, int i, int j, int oldColor, int newColor){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != oldColor || oldColor == newColor) return ;
 
         grid[i][j] = newColor;
 
         for(int k = 0; k < 4; k++){
-            dfs(grid, i + dx[k], j + dy[k],oldColor, newColor);
+            dfs(grid, i + dx[k], j + dy[k], oldColor, newColor);
         }
     }
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int oldColor = image[sr][sc];
-        if(oldColor == color) return image;
         dfs(image, sr, sc, oldColor, color);
         return image;
     }
