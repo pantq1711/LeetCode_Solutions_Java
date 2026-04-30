647960f87faf62b46136a067fcf4c2eb5c93aff0
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-04-30 09:31:49 +0700

    Time: 2 ms (63.04%), Space: 46.7 MB (22.61%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,16 +1,17 @@
 class Solution {
-        static int[] dx = {-1, 1, 0, 0};
-        static int[] dy = {0, 0, -1, 1};
-    private int dfs(int[][] grid, int i, int j){
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public int dfs(int[][] grid, int i, int j){
+        int area = 0;
         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return 0;
-        int cnt = 1;
 
         grid[i][j] = 0;
+        area++;
 
         for(int k = 0; k < 4; k++){
-            cnt += dfs(grid, i + dx[k], j + dy[k]);
+            area += dfs(grid, i + dx[k], j + dy[k]);
         }
-        return cnt;
+        return area;
     }
 
     public int maxAreaOfIsland(int[][] grid) {
@@ -20,7 +21,7 @@ class Solution {
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == 1){
-                    res = Math.max(dfs(grid, i, j), res);
+                    res = Math.max(res,dfs(grid, i, j));
                 }
             }
         }
