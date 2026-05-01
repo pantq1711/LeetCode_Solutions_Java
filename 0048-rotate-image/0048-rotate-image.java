88a05d8a9ca38aea62bcc7c3bd441be4165f0a62
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-05-02 05:07:48 +0700

    Time: 0 ms (100%), Space: 43.6 MB (84.77%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,6 +1,8 @@
 class Solution {
+
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
                 int tmp = matrix[i][j];
@@ -8,12 +10,12 @@ class Solution {
                 matrix[j][i] = tmp;
             }
         }
-
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n/2; j++){
+            for(int j = 0; j < n / 2; j++){
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - j - 1];
-                matrix[i][n - j - 1] = tmp;
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
             }
         }
     }
