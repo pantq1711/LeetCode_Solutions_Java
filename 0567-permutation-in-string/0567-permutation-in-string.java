893fa0671da7a2690375c3089075b04050d3bae5
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-05-05 22:39:14 +0700

    Time: 8 ms (53.21%), Space: 43.8 MB (93.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length(), len2 = s2.length();
-        if(len1 > len2) return false;
+        if(s1.length() > s2.length()) return false;
         int[] cnt1 = new int[26];
         int[] cnt2 = new int[26];
         for(int i = 0; i < s1.length(); i++){
             cnt1[s1.charAt(i) - 'a']++;
             cnt2[s2.charAt(i) - 'a']++;
         }
-        for(int i = len1; i < len2; i++){
+        for(int i = s1.length(); i < s2.length(); i++){
             if(Arrays.equals(cnt1, cnt2)) return true;
             cnt2[s2.charAt(i) - 'a']++;
-            cnt2[s2.charAt((i) - len1) - 'a']--;
+            cnt2[s2.charAt(i - s1.length()) - 'a']--;
         }
-
         return Arrays.equals(cnt1, cnt2);
-        
     }
 }
