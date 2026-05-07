37bbf4ab9ebd0d730e11b68ac3698a5d5be456c1
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-05-07 17:17:46 +0700

    Time: 1 ms (81.21%), Space: 43.7 MB (7.37%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,43 +1,16 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-        int cntS = 0;
-        int cntT = 0;
-        while(i >= 0 || j >= 0){
-            while(i >= 0){
-                if(s.charAt(i) == '#'){
-                    cntS++;
-                    i--;
-                }
-                else if(cntS > 0){
-                    i--;
-                    cntS--;
-                }
-                else {
-                    break; //xoa xong
-                }
+    public String backSpacing(String s){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(!res.isEmpty() && s.charAt(i) == '#'){
+                res = new StringBuilder(res.substring(0, res.length() - 1));
             }
-            while(j >= 0){
-                if(t.charAt(j) == '#'){
-                    cntT++;
-                    j--;
-                }
-                else if(cntT > 0){
-                    j--;
-                    cntT--;
-                }
-                else {
-                    break;
-                } 
-            }
-            if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) return false;
-if ((i >= 0) != (j >= 0)) {
-                return false;
-            }
-            i--;
-            j--;
+            else if(s.charAt(i) != '#') res.append(s.charAt(i));
         }
-        return true;
+        return res.toString();
+    }
+
+    public boolean backspaceCompare(String s, String t) {
+        return backSpacing(s).equals(backSpacing(t));
     }
 }
