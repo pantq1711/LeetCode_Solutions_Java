74f1101067c8018d9acec265acd44b28a9c5d472
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-05-10 15:40:35 +0700

    Time: 2 ms (99.91%), Space: 45.8 MB (69.67%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,27 +1,27 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int count = 0;
-        int[] cnt = new int[128];
-        int minLen = Integer.MAX_VALUE;
-        int startIndex = 0;
+        int[] cnt = new int[256];
         for(char c : t.toCharArray()){
             cnt[c]++;
         }
+        int count = 0;
         int left = 0;
+        int start = 0;
+        int len = Integer.MAX_VALUE;
         for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
             cnt[c]--;
             if(cnt[c] >= 0) count++;
             while(count == t.length()){
-                if(minLen > right - left + 1){
-                    minLen = right - left + 1;
-                    startIndex = left;
+                if(right - left + 1 < len){
+                    len = right - left + 1;
+                    start = left;
                 }
                 cnt[s.charAt(left)]++;
                 if(cnt[s.charAt(left)] == 1) count--;
                 left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLen);
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
 }
