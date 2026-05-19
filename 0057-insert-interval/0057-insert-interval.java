71bb14ac552371be887002208e6dd6aac707b76c
pantq1711 <143337247+pantq1711@users.noreply.github.com>
2026-05-19 11:15:36 +0700

    Time: 1 ms (98.03%), Space: 47.3 MB (18.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,20 +3,25 @@ public int[][] insert(int[][] intervals, int[] newInterval) {
     List<int[]> res = new ArrayList<>();
     int left = 0;
     int n = intervals.length;
-    while(left < n && intervals[left][1] < newInterval[0]){
-        res.add(intervals[left]);
-        left++;
+    while(left < n && intervals[left][1] < newInterval[0] ){
+            res.add(intervals[left]);
+            left++;
     }
     while(left < n && intervals[left][0] <= newInterval[1]){
-        newInterval[0]  = Math.min(intervals[left][0], newInterval[0]);
-        newInterval[1]  = Math.max(intervals[left][1], newInterval[1]);
-        left++;
+            newInterval[0] = Math.min(newInterval[0], intervals[left][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[left][1]);
+            left++;
     }
     res.add(newInterval);
-    while(left < n && intervals[left][0] > newInterval[1]){
-        res.add(intervals[left]);
-        left++;
+    while(left < n){
+        res.add(intervals[left++]);
     }
-    return res.toArray(new int[res.size()][]);
+        return res.toArray(new int[res.size()][]); 
 }
+
+
+
+
+
+
 }
